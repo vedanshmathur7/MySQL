7d0584d06500b5aa56828a42b15f10b9f214f522
Vedansh Mathur <vedanshmathur007@gmail.com>
2025-06-08 15:11:42 +0530

    Update vedansh_sql.sql

--- vedansh_sql.sql
+++ vedansh_sql.sql
@@ -1,15 +1,22 @@
-create database vedansh;
--- drop database vedansh;
-create database if not exists vedansh ; 
+select * from student;
 
-use vedansh ;
-create table rajesh (
-st_id int primary key,
-name varchar (100),
-age int not null
-);
-insert into rajesh
+insert into student (name, rollno, marks)
 values 
-(1, 'Vedansh', 18),
-(2, 'Tanish', 21);
-select * from rajesh;
+('Vedansh', 240686, 56),
+('Tanish', 240656, 89);
+
+create table course (
+id_no int primary key,
+course varchar (50)
+);
+
+insert into course 
+values
+(240686, 'Maths'),
+(240656, 'Science');
+
+select * from course as c
+inner join student as s
+on s.rollno = c.id_no;
+
+
